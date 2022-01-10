6a17119aefa377b674afea1c53da9793f627f16f
sandip rathore <sandiprathore2910@gmail.com>
2022-01-11 00:21:15 +0530

    Python Program to Add Two Numbers

---
+++ mathematical_problems/add_two_numbers.py
@@ -0,0 +1,5 @@
+num_1=int(input("enter the first number: "))
+num_2=int(input("enter the first second: "))
+sum=num_1+num_2
+print("the sum of {0} and {1} is {2}".format(num_1 , num_2, sum))
+print("the sum of %0.0f and %0.0f is %0.0f "%(num_1,num_2,sum))
