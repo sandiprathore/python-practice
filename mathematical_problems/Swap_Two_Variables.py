6b7090b177715d01198446ef05da6acd43b5de8e
sandip rathore <sandiprathore2910@gmail.com>
2022-01-11 00:24:51 +0530

    python program to to swap two variables

---
+++ mathematical_problems/Swap_Two_Variables.py
@@ -0,0 +1,7 @@
+num_1=int(input("enter the first number "))
+num_2=int(input("enter the second number "))
+print("the value of first and second number before swaping is %0.0f, %0.0f"%(num_1,num_2))
+swaper=num_2
+num_2= num_1
+num_1=swaper
+print("the value of first and second number after swaping is %0.0f, %0.0f"%(num_1,num_2))
