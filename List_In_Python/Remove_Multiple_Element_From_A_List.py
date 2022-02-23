f1e45686411fc61b533d8407bfc3e372c66a0c31
sandip rathore <sandiprathore2910@gmail.com>
2022-02-23 22:53:50 +0530

    python program remove multiple element from a list

---
+++ List_In_Python/Remove_Multiple_Element_From_A_List.py
@@ -0,0 +1,6 @@
+# remove multiple element from a list 
+test_list = [1,2,8,3,5]
+want_to_remove = [1,5]
+for ele in want_to_remove :
+    test_list.remove(ele)
+print(f'updated list : {test_list}') 
