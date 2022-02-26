360e804039fa9232a8f90c692728d809ebbf57c8
sandip rathore <sandiprathore2910@gmail.com>
2022-02-26 06:57:07 +0530

    Python program to find second largest number in a list

---
+++ List_In_Python/Find_Second_Largest_Number_In_A_List.py
@@ -0,0 +1,7 @@
+# Python program to find second largest number in a list 
+
+
+test_list = [ 4, 6, 3 ]
+test_list.sort()
+second_largest_number = test_list[-2] 
+print("smallest number in a list is: ",second_largest_number)
